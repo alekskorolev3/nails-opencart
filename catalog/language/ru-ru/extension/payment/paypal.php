d00adfb16dddcc40c2067bd1ae62292c45c37f3d
alekskorolev3 <alekskorolev3@gmail.com>
2024-04-02 12:06:22 +0300

    Initial commit

---
+++ catalog/language/ru-ru/extension/payment/paypal.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_title']						= 'PayPal (Express, Карты Visa и MasterCard)';
+$_['text_paypal_express']				= 'PayPal Express';
+$_['text_paypal_card']					= 'PayPal Card';
+$_['text_wait']							= 'Подождите пожалуйста!';
+$_['text_order_message']				= 'PayPal Seller Protection - %s';
+
+// Entry
+$_['entry_card_number']					= 'Номер карты';
+$_['entry_expiration_date']				= 'Дата срока действия карты';
+$_['entry_cvv']							= 'Код безопасности (CVV)';
+
+// Button
+$_['button_pay']						= 'Оплатить картой';
+
+// Error
+$_['error_warning']					= 'Пожалуйста, внимательно проверьте форму на наличие ошибок.';
+$_['error_3ds_error']				= 'Произошла ошибка с системой аутентификации 3DS.';
+$_['error_3ds_skipped_by_buyer']	= '3D Secure authentication была пропущена вами.';
+$_['error_3ds_failure']				= 'Возможно, вы не выполнили вызов или устройство не было проверено.';
+$_['error_3ds_undefined']			= 'Карточная сеть не требовала 3D Secure.';
+$_['error_3ds_bypassed']			= '3D Secure был пропущен, так как система аутентификации не требовала вызова.';
+$_['error_3ds_unavailable']			= 'Банк-эмитент не может завершить аутентификацию.';
+$_['error_3ds_attempted']			= 'Карта не зарегистрирована в 3D Secure, так как банк-эмитент карты не участвует в 3D Secure.';
+$_['error_3ds_card_ineligible']		= 'Карта не подходит для безопасной аутентификации 3DS.';
+$_['error_payment']					= 'Пожалуйста, выберите другой способ оплаты или <a href="%s" target="_blank">свяжитесь с нами</a>.';
+$_['error_timeout']					= 'Извините, PayPal в данный момент занят. Пожалуйста, попробуйте еще раз позже!';
