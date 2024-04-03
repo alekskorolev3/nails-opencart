4e540203a9d4c619db3967a449051b3fe3f03d17
alekskorolev3 <alekskorolev3@gmail.com>
2024-04-03 14:43:38 +0300

    Init

---
+++ catalog/language/ru-ru/extension/module/paypal_smart_button.php
@@ -0,0 +1,75 @@
+<?php
+// Text
+$_['text_title']						= 'PayPal';
+$_['text_checkout_payment_address']  	= 'Детали Оплаты';
+$_['text_checkout_shipping_address'] 	= 'Детали Доставки';
+$_['text_checkout_shipping_method']  	= 'Способ Доставки';
+$_['text_checkout_payment_method']  	= 'Способ Оплаты';
+$_['text_your_details']              	= 'Персональная информация';
+$_['text_your_address']              	= 'Ваш Адрес';
+$_['text_cart']               			= 'Корзина';
+$_['text_shipping_updated']   			= 'Обновлена служба доставки';
+$_['text_order_message']				= 'PayPal Seller Protection - %s';
+$_['text_day']                 			= 'день';
+$_['text_week']                			= 'неделя';
+$_['text_semi_month']          			= 'пол-месяца';
+$_['text_month']               			= 'месяц';
+$_['text_year']                			= 'год';
+$_['text_trial']               			= '%s каждый %s %s для %s платежей ';
+$_['text_recurring']          			= '%s каждый %s %s';
+$_['text_payment_cancel']      			= 'пока не отменено';
+$_['text_recurring_item']      			= 'Повторяющиеся платежи';
+$_['text_payment_recurring']   			= 'Платежный профиль';
+$_['text_trial_description']   			= '%s каждый %d %s(s) для %d платежей';
+$_['text_payment_description'] 			= '%s каждый %d %s(s) для %d платежей';
+$_['text_payment_cancel']      			= '%s каждый %d %s(s) пока не отменено';
+
+// Column
+$_['column_image']             			= 'Изображение';
+$_['column_name']              			= 'Название товара';
+$_['column_model']             			= 'Модель';
+$_['column_quantity']          			= 'Количество';
+$_['column_price']             			= 'Цена за единицу';
+$_['column_total']             			= 'Всего';
+
+// Entry
+$_['entry_email']                    	= 'E-Mail';
+$_['entry_firstname']                	= 'Имя';
+$_['entry_lastname']                 	= 'Фамилия';
+$_['entry_telephone']               	= 'Телефон';
+$_['entry_company']                  	= 'Компания';
+$_['entry_address_1']                	= 'Адрес 1';
+$_['entry_address_2']                	= 'Адрес 2';
+$_['entry_postcode']                 	= 'Индекс';
+$_['entry_city']                     	= 'Город';
+$_['entry_country']                  	= 'Страна';
+$_['entry_zone']                    	= 'Регион / Область';
+
+// Button
+$_['button_confirm']  					= 'Подтвердить';
+$_['button_shipping'] 					= 'Обновить доставку';
+
+// Error
+$_['error_stock']				= 'Продукты, отмеченные знаком***, не доступны в нужном количестве или отсутствуют на складе!';
+$_['error_minimum']				= 'Минимальная сумма заказа для %s равна %s!';
+$_['error_required']			= '%s обязателен!';
+$_['error_product']				= 'Внимание: В вашей корзине нет товаров!';
+$_['error_recurring_required']	= 'Пожалуйста, выберите повторяющийся платеж!';
+$_['error_unavailable']			= 'Пожалуйста, используйте полное оформление с этим заказом!';
+$_['error_shipping']			= 'Внимание: Требуется выбрать способ доставки!';
+$_['error_no_shipping']			= 'Внимание: Никакие варианты доставки не доступны.';
+$_['error_firstname']			= 'Имя должно быть от 1 до 32 символов!';
+$_['error_lastname']			= 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']				= 'Адрес электронной почты не действительный!';
+$_['error_telephone']			= 'Телефон должен быть от 3 до 32 символов!';
+$_['error_password']			= 'Пароль должен быть от 4 до 20 символов!';
+$_['error_confirm']				= 'Подтверждение пароля не соответствует паролю!';
+$_['error_address_1']			= 'Адрес 1 должен быть от 3 до 128 символов!';
+$_['error_city']				= 'Город должен быть от 2 до 128 символов!';
+$_['error_postcode']			= 'Почтовый индекс должен быть от 2 до 10 символов!';
+$_['error_country']				= 'Пожалуйста, выберите страну!';
+$_['error_zone']				= 'Пожалуйста, выберите регион / область!';
+$_['error_agree']				= 'Внимание: Вы должны согласиться с %s!';
+$_['error_address']				= 'Внимание: Вы должны выбрать адрес!';
+$_['error_custom_field']		= '%s обязателен!';
+$_['error_timeout']				= 'Извините, PayPal в данный момент занят. Пожалуйста, попробуйте еще раз позже!';
