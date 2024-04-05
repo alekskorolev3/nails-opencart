f72adb70ea40cfba8ca9bc626c7207c6a91c9d52
GorbachyovKS <gorbachyovKS@gmail.com>
2024-04-05 15:28:16 +0300

    product page

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -249,10 +249,12 @@ class ControllerProductProduct extends Controller {
 			$this->load->model('catalog/review');
 
 			$data['tab_review'] = sprintf($this->language->get('tab_review'), $product_info['reviews']);
-
+			
 			$data['product_id'] = (int)$this->request->get['product_id'];
 			$data['manufacturer'] = $product_info['manufacturer'];
 			$data['manufacturers'] = $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $product_info['manufacturer_id']);
+			$data['weight'] = $product_info['weight'];
+			$data['weight_class_id'] = $product_info['weight_class_id'];
 			$data['model'] = $product_info['model'];
 			$data['reward'] = $product_info['reward'];
 			$data['points'] = $product_info['points'];
@@ -435,6 +437,8 @@ class ControllerProductProduct extends Controller {
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')),
 					'price'       => $price,
 					'special'     => $special,
+					'weight'           => $result['weight'],
+					'weight_class_id'  => $result['weight_class_id'],
 					'tax'         => $tax,
 					'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
 					'rating'      => $rating,
